3df998d631d1bb3eb4d526fff837b2cdca85f723
kquach31 <quachkelvin31@gmail.com>
2026-03-29 16:58:38 -0700

    added second part to finalize M6

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,37 @@
 import express from "express";
+import session from "express-session";
 import path from "path";
 
 import homeRoutes from "./routes/home.js";
 import testRoutes from "./routes/test.js";
+
+import connectPgSimple from "connect-pg-simple";
+import db from "./db/connections.js";
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const app = express();
 const PORT: number = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const PgSession = connectPgSimple(session);
+app.use(
+  session({
+    store: new PgSession({ pgPromise: db }),
+    secret: process.env.SESSION_SECRET || "dev-secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  }),
+);
 
 console.log(path.join(path.resolve(), "../public"));
 app.use(express.static(path.join(path.resolve(), "../public")));
